9124aa4ec0f36e751810c527db244b8825156a15
Owner <leshagl@yandex.ru>
2022-06-02 19:42:31 +0300

    do create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,20 @@
+import * as fs from 'fs';
+
 export const create = async () => {
     // Write your code here 
+    const path = "./src/fs/files/fresh.txt";
+    const error = new Error('FS operation failed');
+    fs.access(path, function(err){
+        if(err == null) {
+            throw new Error("FS operation failed")
+        } else {
+            fs.writeFile(path, 'I am fresh and young', function(err) {
+                if(err) throw err
+            })
+        }
+    }) 
+
+    
 };
+
+create()
