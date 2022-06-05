531fba71f283c858bbe1c2a115840981bdcdb095
Owner <leshagl@yandex.ru>
2022-06-05 23:45:22 +0300

    fix wt

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,19 @@
+import cp  from 'child_process';
+
 export const spawnChildProcess = async (args) => {
     // Write your code here
+
+    cp.exec(`node "./src/cp/files/script.js" args`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return;
+          }
+        console.log( stdout)
+    } )
 };
+
+spawnChildProcess()
--- src/wt/main.js
+++ src/wt/main.js
@@ -3,31 +3,40 @@ import os from 'os'
 
 export const performCalculations = async () => {
     let result = []
-    let workers= []
+    let workers
+    let promises = []
     // Write your code here
  //   const worker = new threads.Worker('./src/wt/worker.js',{workerData: 5})
     const cpu = os.cpus().length
     for (let i = 0; i < cpu; i++){
       
-        workers[i] = new threads.Worker('./src/wt/worker.js', {workerData: 10+i})
+        workers = new threads.Worker('./src/wt/worker.js', {workerData: 10+i})
 
-        workers[i].once('message', res =>{
-            result[i]= ({
-                status: 'resolved',
-                data: res               
-            });
-            if(result.length==cpu) console.log(result)
-                  
-        })
-        
-
-        
-        
+        promises.push(
+            new Promise((resolve, reject) => {
+            
+            workers.on('message', res =>{
+                resolve(res)            
+                }).on('error', (err) => {
+                    reject(err)
+                })
+  //              if(result.length==cpu) console.log(result)
+                      
+            }))
+               
         
     }
- 
 
-
+    result = await Promise.allSettled(promises)
+    
+    result.forEach((response)=>{
+        if(response.status == "fulfilled") response.status = 'resolved'
+        if(response.status == 'rejected') {
+            response.status = 'error'
+            response.num = null
+        }
+    })
+    console.log(result)
 
 };
 
